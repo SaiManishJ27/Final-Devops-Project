ebfdac09ddd739b70a9f7c22783cd704704a98b8
SaiManishJ27 <167736716+SaiManishJ27@users.noreply.github.com>
2024-11-09 19:19:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React with Jenkins, Docker, and Kubernetes!</h1>
+    </div>
+  );
+}
+
+export default App;
